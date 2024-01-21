01f7eed5a3f0ad429060b0db9469b4fcd93b2258
noe <noe.jaraq@gmail.com>
2024-01-21 18:49:18 -0500

    new routes

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
+
+app.get('/post', (req, res) => {
+    res.render('post.ejs');
+});
+
+app.get('/create', (req, res) => {
+    res.render('create.ejs');
+});
+
+app.get('/my-posts', (req, res) => {
+    res.render('my-posts.ejs');
+});
+
+app.post('/create', (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    const image = req.body.image;
+
+    console.log(title, content, image);
+    res.redirect('/');
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
